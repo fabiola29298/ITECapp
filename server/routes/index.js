3f3dd938c5c6a5c6ea8a887500f4d2dc6ef03e81
Fabiola <fabiola29298@gmail.com>
2019-08-17 01:00:41 -0400

    add route login  routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,5 @@ app.use(require('./person'));
 app.use(require('./activity'));
 app.use(require('./notification'));
 app.use(require('./sponsor'));
-
+app.use(require('./login'));
 module.exports = app;
